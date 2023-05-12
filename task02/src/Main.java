b84c7254f996bf0f754b5559be45917b287104e5
jylmarcus <maplecow@gmail.com>
2023-05-12 11:49:42 +0800

    Added textAnalyzer functions

--- task02/src/Main.java
+++ task02/src/Main.java
@@ -1,7 +1,34 @@
 package src;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         
+        Map<String, HashMap<String,Float>> wordMap = new HashMap<String, HashMap<String, Float>>();
+
+        //start loop
+        //import file
+        String text = textAnalyzer.importFile(new File(args[0]));
+        //clean file
+        String[] cleanedText = textAnalyzer.cleanText(text);
+        //map text
+        wordMap = textAnalyzer.mapWords(cleanedText, wordMap);
+        //loop
+
+        //analyze text
+        Map<String, HashMap<String, Float>> avgMap = new HashMap<String, HashMap<String, Float>>();
+        avgMap = textAnalyzer.analyzeText(wordMap);
+
+        for (String word : avgMap.keySet()) {
+            System.out.println(word);
+            for(String nextWord: avgMap.get(word).keySet()){
+                System.out.println("    " + nextWord + " " + avgMap.get(word).get(nextWord));
+            }
+          }
     }
 }
---
+++ task02/src/textAnalyzer.java
@@ -0,0 +1,80 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class textAnalyzer {
+
+    public textAnalyzer(String path) {
+        
+    }
+
+    public static String importFile(File file) throws IOException {
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+
+        while((line = br.readLine())!=null) {
+            sb.append(line);
+            sb.append(" ");
+        }
+
+        br.close();
+        fr.close();
+
+        return sb.toString();
+    }
+
+    public static String[] cleanText(String text) {
+        text = text.replace("-", " ");
+        String[] cleanedText = text.replaceAll("[^a-zA-Z ]", "").toLowerCase().trim().split(" ");
+        return cleanedText;
+    }
+
+    public static Map<String, HashMap<String, Float>> mapWords(String[] text, Map<String, HashMap<String, Float>> wordMap) {
+        for(int i = 0; i < text.length-1; i++) {
+            HashMap<String, Float> tempNextWordMap = new HashMap<String, Float>();
+            if(!wordMap.containsKey(text[i])){
+                tempNextWordMap.put(text[i+1], 1f);
+                wordMap.put(text[i], tempNextWordMap);
+            } else {
+                wordMap.get(text[i]).computeIfAbsent(text[i+1], v -> 0f);
+                wordMap.get(text[i]).computeIfPresent(text[i+1], (k, v) -> v += 1f);
+            }
+        }
+        return wordMap;
+    }
+
+    public static Map<String,HashMap<String,Float>> analyzeText(Map<String, HashMap<String, Float>> wordMap) {
+
+        Map<String, HashMap<String, Float>> avgMap = new HashMap<String, HashMap<String, Float>>();
+        for (String word : wordMap.keySet()){
+            HashMap<String, Float> nextWordsMap = wordMap.get(word);
+            HashMap<String, Float> tempMap = new HashMap<String, Float>();
+            Float nextWordCount = 0f;
+
+            //get the sum of nextwords
+            for(Float i : nextWordsMap.values()) {
+                nextWordCount += i;
+            }
+
+            //put the nextWord and corresponding avg value into tempMap
+            for(String nextWord: nextWordsMap.keySet()) {
+                float avg = nextWordsMap.get(nextWord)/nextWordCount;
+                tempMap.put(nextWord, avg);
+            }
+
+            //put the completed key and tempMap pair into avgMap
+            avgMap.put(word, tempMap);
+        }
+        return avgMap;
+    }
+}
